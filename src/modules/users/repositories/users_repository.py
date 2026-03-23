61bc17f215304e6e410c9bdd4fc65df0b8264a7f
plox1237 <mcastram@gmail.com>
2026-03-23 00:02:14 -0500

    AÑADIDO NOMBRE DE ROL JUNTO A LA CONSULTA DE LOS USUARIOS

--- src/modules/users/repositories/users_repository.py
+++ src/modules/users/repositories/users_repository.py
@@ -8,8 +8,21 @@ class UsersRepository:
     async def get_all_users(self):
         try:
             statement = select(User).order_by(User.user_id)
-            results = self.session.exec(statement)
-            return results.all()
+            results = self.session.exec(statement).all()
+            return [
+            {
+                "user_id": u.user_id,
+                "name": u.name,
+                "last_name": u.last_name,
+                "email": u.email,
+                "phone": u.phone,
+                "role": {
+                    "role_id": u.role.role_id,
+                    "name": u.role.name
+                }
+            }
+            for u in results
+        ]
         except Exception as e:
             self.session.rollback()
             raise e
@@ -17,8 +30,18 @@ class UsersRepository:
     async def get_user_by_id(self, user_id: int):
         try:
             statement = select(User).where(User.user_id == user_id)
-            result = self.session.exec(statement)
-            return result.first()
+            result = self.session.exec(statement).first()
+            return {
+                "user_id": result.user_id,
+                "name": result.name,
+                "last_name": result.last_name,
+                "email": result.email,
+                "phone": result.phone,
+                "role": {
+                    "role_id": result.role.role_id,
+                    "name": result.role.name
+                }
+            }
         except Exception as e:
             self.session.rollback()
             raise e
@@ -26,8 +49,18 @@ class UsersRepository:
     async def get_user_by_email(self, email: str):
         try:
             statement = select(User).where(User.email == email)
-            result = self.session.exec(statement)
-            return result.first()
+            result = self.session.exec(statement).first()
+            return {
+                "user_id": result.user_id,
+                "name": result.name,
+                "last_name": result.last_name,
+                "email": result.email,
+                "phone": result.phone,
+                "role": {
+                    "role_id": result.role.role_id,
+                    "name": result.role.name
+                }
+            }
         except Exception as e:
             self.session.rollback()
             raise e
@@ -35,8 +68,18 @@ class UsersRepository:
     async def get_user_by_phone(self, phone: str):
         try:
             statement = select(User).where(User.phone == phone)
-            result = self.session.exec(statement)
-            return result.first()
+            result = self.session.exec(statement).first()
+            return {
+                "user_id": result.user_id,
+                "name": result.name,
+                "last_name": result.last_name,
+                "email": result.email,
+                "phone": result.phone,
+                "role": {
+                    "role_id": result.role.role_id,
+                    "name": result.role.name
+                }
+            }
         except Exception as e:
             self.session.rollback()
             raise e
