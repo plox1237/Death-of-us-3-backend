d5ad533bd33e29155b3b1b348a6da5f5c909aede
Plox1237 <mcastram@gmail.com>
2026-04-22 15:02:46 -0500

    MODIFICADO INICIO DE LA APLICACION PARA DESPLIEGUE EN LA NUBE

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.config.database import start_database
 from src.routes.router import router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    start_database()
+    print("INICIANDO APLICACION")
+    yield
+    print("APAGANDO APLICACION")
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
