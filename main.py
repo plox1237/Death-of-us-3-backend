13e2818900bec78d6938c5008b98a73dfb85f5a8
Plox1237 <mcastram@gmail.com>
2026-04-22 15:22:04 -0500

    MODIFICADO MANEJO DE ARRANQUE DE LA APP PARA DESPLIEGUE

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
 from app import app
 
+load_dotenv()
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4000) # uvicorn main:app --host 0.0.0.0 --port 4000
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
